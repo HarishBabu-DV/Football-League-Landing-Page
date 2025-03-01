203bbc47187c7391bfda9ef2228c7d3ff2807124
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-01 20:55:00 +0530

    created football teams in asset.js

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -1,5 +1,9 @@
 import logo from './images/superleague.png'
 import vip2 from './images/i.m.vijayan.png'
+import logowhite from './images/logo(white).png'
+import kannursquadfc from './images/kannur-squad-fc.jpg';
+import thiruvandrumkombans from './images/thiruvandrumkombans.png'
+import thrissurroar from './images/thrissurroar.jpg'
 const firstnavItems=[
     {
         id:1,
@@ -24,4 +28,43 @@ const secondNavItems=[
         title:"Contact"
     }
 ]
-export {logo,vip2,firstnavItems,secondNavItems}
+
+const footBallTeams=[
+    {
+        id:1,
+        imageUrl:"https://upload.wikimedia.org/wikipedia/en/c/c7/Calicut_FC.png",
+        teamName:"calicut football club"
+    },
+    {
+        id:2,
+        imageUrl:kannursquadfc,
+        teamName:"kannur squad fc"
+    },
+    {
+        id:3,
+        imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXbF99bsGRlu5_oiMsLztuQTiIhJSVJoEm0V1tAduHOydFXJDMXKFwTrbkTLQCGEP0_dg&usqp=CAU",
+        teamName:"kochi football club"
+    },
+    {
+        id:4,
+        imageUrl:"https://fcmalappuram.com/wp-content/uploads/2024/07/Untitled-design-31.png",
+        teamName:"malappuram football club"
+    },
+    {
+        id:5,
+        imageUrl:"https://fcmalappuram.com/wp-content/uploads/2024/07/Untitled-design-31.png",
+        teamName:"malappuram football club"
+    },
+    {
+        id:6,
+        imageUrl:thiruvandrumkombans,
+        teamName:"thiruvananthapuram kombans fc"
+    },
+    {
+        id:7,
+        imageUrl:thrissurroar,
+        teamName:"thrissur roar fc"
+    }
+]
+
+export {logo,vip2,logowhite,firstnavItems,secondNavItems,footBallTeams}
--- src/sections/LandingPage Sections/ChallengesSection.jsx
+++ src/sections/LandingPage Sections/ChallengesSection.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
+import { logowhite } from '../../assets/asset'
 
 const ChallengesSection = () => {
   return (
     <section className='w-[90%] mx-auto h-dvh bg-red-900'>
 
         {/* Challenges Heading  */}
-        <div className='bg-white h-[8vh] challenges-heading-container flex justify-center items-center'>
+        <div className='bg-white h-[8%] challenges-heading-container flex justify-center items-center'>
             <h2 className='uppercase text-center text-[1.6rem] challenges-heading'>challenges</h2>
         </div>
         {/* Super League Kerala  */}
-        <div className='bg-white'>
-            
+        <div className= 'w-full h-[92%] bg-yellow-100'>
             {/* Super League Kerala Scouting phase heading*/}
             <h2 className='capitalize flex justify-center font-semibold text-[1.7rem] scouting-phase-heading'>
                 super league kerala scouting phase
             </h2>
 
-            {/* Super League kerala Teams  */}
-            <div className='super-league-kerala-teams-container'>
+            {/* Super League kerala Teams Container */}
+            <div className='w-full h-[60%] super-league-kerala-teams-container rounded-lg'>
                 
+                {/* Super league kerala logo container  */}
+                <div>
+                    <img src={logowhite} alt="" />
+                </div>
             </div>
             <div></div>
         </div>
