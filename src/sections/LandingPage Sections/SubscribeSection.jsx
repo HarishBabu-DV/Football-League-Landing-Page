6871ee6b8252c35eefb60ec9a18dd61cb7eb6c2f
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-02 18:53:47 +0530

    modified subscribe section

--- src/sections/LandingPage Sections/SubscribeSection.jsx
+++ src/sections/LandingPage Sections/SubscribeSection.jsx
@@ -4,16 +4,16 @@ const SubscribeSection = () => {
   return (
     <section className='w-full min-h-dvh subscribe-section-container'>
         {/* Quotes Description */}
-        <div>
-            <p className='quotes-line1'>Stay in the loop. Get the latest updates from SLK.</p>
-            <h3 className='quotes-line2'>
-                <span>Stay</span>
-                <span>Connected</span>
+        <div className='flex flex-col justify-center gap-y-2 '>
+            <p className='text-[1rem] quotes-line1'>Stay in the loop. Get the latest updates from SLK.</p>
+            <h3 className='font-medium text-3xl quotes-line2'>
+                <span className='quotes-line2-stay'>Stay</span>
+                <span className='quotes-line2-connected'>Connected</span>
             </h3>
-            <h1 className='super-league-kerala-heading'>Super League Kerala</h1>
+            <h1 className='font-semibold text-5xl super-league-kerala-heading'>Super League Kerala</h1>
         </div>
         {/* Email input  */}
-        <input type="email" name='email' id='email' placeholder='Email' className=''/>
+        <input type="email" name='email' id='email' placeholder='Email' className='bg-[#bcbcbc] rounded-md px-4 py-3 text-[#171717] w-[50%]'/>
         
         {/* Subscribe us button */}
         <input type="submit" value="Subscribe us" className=''/>
